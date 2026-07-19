2d15fe75d4e4ea58131c4c479eaf97d208849e98
AayushAnand07 <aayush.s.2014@gmail.com>
2026-07-19 12:44:23 +0530

    completing orderResponse dto

--- src/main/java/org/gateway/paygate/payment/dto/response/OrderResponse.java
+++ src/main/java/org/gateway/paygate/payment/dto/response/OrderResponse.java
@@ -1,6 +1,21 @@
 package org.gateway.paygate.payment.dto.response;
 
-public record OrderResponse(
+import org.gateway.paygate.common.entity.Money;
+import org.gateway.paygate.common.enums.OrderStatus;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+public record OrderResponse(
+    UUID Id,
+    UUID merchantId,
+    String receipt, //Merchant Reference No.
+    Money amount,
+    OrderStatus status,
+    Integer attempts,
+    Map<String, Object> notes,
+    LocalDateTime expiresAt,
+    LocalDateTime createdAt
 ) {
 }
