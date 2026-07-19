c57141093786965602a6a52b9caac24c3d2a5834
AayushAnand07 <aayush.s.2014@gmail.com>
2026-07-19 12:44:55 +0530

    adding jpa methods

--- src/main/java/org/gateway/paygate/payment/repository/OrderRepository.java
+++ src/main/java/org/gateway/paygate/payment/repository/OrderRepository.java
@@ -1,11 +1,17 @@
 package org.gateway.paygate.payment.repository;
 
+import jakarta.validation.constraints.Size;
+import org.gateway.paygate.payment.dto.response.OrderResponse;
 import org.gateway.paygate.payment.entity.OrderRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<OrderRecord, UUID> {
 
 
+    boolean existsByMerchantIdAndReceipt(UUID merchantId, @Size(max = 100) String receipt);
+
+    Optional<OrderRecord> findByIdAndMerchantId(UUID orderId, UUID merchantId);
 }
